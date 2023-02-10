90c75a94d1e9d2d8a73f3c6bc8a8993d1efebd9a
LeoZhang43 <yzhang43@usc.edu>
2023-02-10 13:45:03 -0800

    update 2D parity

--- crc_vs_parity2d.cc
+++ crc_vs_parity2d.cc
@@ -1,19 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+string add_digit(string input) {
+    string output = "";
+    for (int i = 0; i < input.length(); i += 8) {
+        int count = 0;
+        for (int j = i; j < i + 8; j++) {
+            if (input[j] == '1') count++;
+        }
+        if (count % 2 == 0) {
+            output += input.substr(i, 8) + "0";
+        } else {
+            output += input.substr(i, 8) + "1";
+        }
+    }
+    return output;
+}
 
 //get the first string in dataVs.txt file
 string getFirstString(string line) {
     int spaceIndex = line.find(" ");
-    return line.substr(0, spaceIndex);
+    return add_digit(line.substr(0, spaceIndex));
 }
 
 //calculate first and second data with XOR
 string XOR(string first, string second){
 	string answer = "";
-	for(int i = 0;i < 64;i++){
+	for(int i = 0;i < 72;i++){
 		int p1 = first.at(i) - '0';
 		int p2 = second.at(i) - '0';
 		answer += p1 ^ p2 + '0';
@@ -26,18 +43,20 @@ string getSecondString(string line) {
     int spaceIndex = line.find(" ");
     string first = line.substr(0, spaceIndex);
     string second = line.substr(spaceIndex + 1);
-
-
-    string result = XOR(first, second);
+    string modified = add_digit(first);
+    // cout << modified.length() << endl;
+    // cout << second.length() << endl;
+    string result = XOR(modified, second);
+    // cout << result.length() << endl;
     return result;
 }
 
 //do 2D parity check and get col, row in return
-string* convertString(string s) {
-    char arr[8][8];
+vector<string> convertString(string s) {
+    char arr[8][9];
     int index = 0;
     for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
+        for (int j = 0; j < 9; j++) {
             arr[i][j] = s[index];
             index++;
         }
@@ -45,32 +64,24 @@ string* convertString(string s) {
 
     string row = "";
     string col = "";
-    string error_detection = "";
-    int count_error = 0;
+    int num = 0;
     for (int i = 0; i < 8; i++) {
+        row += arr[i][8];
+        if(arr[i][8] == '1') num++;
+    }
+    for (int j = 0; j < 8; j++) {
         int count = 0;
-        for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < 8; i++) {
             if (arr[i][j] == '1') count++;
-        }
-        row += (count % 2 == 0 ? '0' : '1');
-
-        count = 0;
-        for (int j = 0; j < 8; j++) {
-            if (arr[j][i] == '1') count++;
         }
         col += (count % 2 == 0 ? '0' : '1');
     }
-
-    for(int i = 0;i < 8;i++){
-        if(col.at(i) == '1'){
-            count_error++;
-        }
-    }
-    row += (count_error % 2 == 0 ? '0' : '1');
-
-    string * ans = new string[2];
-    ans[0] = row;
-    ans[1] = col;
+    row += (num % 2 == 0 ? '0' : '1');
+    vector<string> ans;
+    ans.push_back(row);
+    ans.push_back(col);
+    // cout << "Row: " << row << endl;
+    // cout << "Col: " << col << endl;
     return ans;
 }
 
@@ -194,27 +205,27 @@ int main() {
     while (getline(file, line)) {
         string First = getFirstString(line);
         string Second = getSecondString(line);
-        //cout << First << endl;
-        //cout << Second << endl;
+        // cout << First << endl;
+        // cout << Second << endl;
         string remainder = encodeData(First, key);
         string ans = mod2div(Second + remainder, key);
         //cout << ans << endl;
-        string* firstAnswer = convertString(First);
-        string* secondAnswer = convertString(Second);
-        cout << "2D parity:" << "Col: " << firstAnswer[1] << "; Row: " << firstAnswer[0] << " ;";
+        vector<string> firstAnswer = convertString(First);
+        vector<string> secondAnswer = convertString(Second);
+        cout << "2D parity:" << "Col: " << firstAnswer[0] << "; Row: " << firstAnswer[1] << " ;";
         if(firstAnswer[0].compare(secondAnswer[0]) == 0 && 
             firstAnswer[1].compare(secondAnswer[1]) == 0){
-            //std::cout << firstAnswer[0] << std::endl;
-            //std::cout << secondAnswer[0] << std::endl;
-            //std::cout << firstAnswer[1] << std::endl;
-            //std::cout << secondAnswer[1] << std::endl;
+            // std::cout << firstAnswer[0] << std::endl;
+            // std::cout << secondAnswer[0] << std::endl;
+            // std::cout << firstAnswer[1] << std::endl;
+            // std::cout << secondAnswer[1] << std::endl;
             std::cout << "        Reuslt: Pass" << std::endl;
         }else{
-            //std::cout << firstAnswer[0] << std::endl;
-            //std::cout << secondAnswer[0] << std::endl;
-            //std::cout << firstAnswer[1] << std::endl;
-            //std::cout << secondAnswer[1] << std::endl;
-            std::cout << "        Reuslt: Not Pass" << std::endl;
+            // cout << firstAnswer[0] << endl;
+            // cout << secondAnswer[0] << endl;
+            // cout << firstAnswer[1] << endl;
+            // cout << secondAnswer[1] << endl;
+            cout << "        Reuslt: Not Pass" << std::endl;
         }
         cout << "CRC :" << remainder << ";";
         if(ans.compare("0000000000000000") == 0){
